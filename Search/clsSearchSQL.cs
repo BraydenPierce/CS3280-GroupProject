a9aa46a0d0406168bb92af7a00092a80dbc221c5
GarrettOtto <garrettotto@mail.weber.edu>
2025-03-29 16:19:38 -0600

    Garrett Otto - Search Window Push

--- Search/clsSearchSQL.cs
+++ Search/clsSearchSQL.cs
@@ -1,13 +1,206 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
 
 namespace GroupProject.Search
 {
     class clsSearchSQL
     {
-        //Doing all SQL inquiries here
+        /*
+           Overall Note:
+           - I tested all queries in Microsoft Access to ensure they work
+           - I got all SQL statements from the Database Help.doc provided from the professor
+        */
+
+        /// <summary>
+        /// This SQL returns a string to get all the invoices
+        /// </summary>
+        /// <returns></returns>
+        public string GetInvoices()
+        {
+            try
+            {
+                //GetInvoices()
+                //  SELECT * FROM Invoices
+                string sSQL = "SELECT * FROM Invoices";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with the Num 5000
+        /// </summary>
+        /// <returns></returns>
+        public string GetInvoiceNum5000()
+        {
+            try
+            {
+                //GetInvoiceNum5000()
+                //  SELECT * FROM Invoices WHERE InvoiceNum = 5000
+                string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = 5000";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with: Num 5000, and Date 4/13/2018
+        /// </summary>
+        /// <returns></returns>
+        public string GetInvoiceNum5000AndDate4_13_2018()
+        {
+            try
+            {
+                //GetInvoiceNum5000AndDate4_13_2018()
+                //  SELECT* FROM Invoices WHERE InvoiceNum = 5000 AND InvoiceDate = #4/13/2018#
+                //Note: Query works but doesn't return anything due to no invoices existing with that information
+                string sSQL = "SELECT* FROM Invoices WHERE InvoiceNum = 5000 AND InvoiceDate = #4/13/2018#";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with: Num 5000, Date 4/13/2018, and Cost 120
+        /// </summary>
+        /// <returns></returns>
+        public string GetInvoiceNum5000AndDate4_13_2018AndCost120()
+        {
+            try
+            {
+                //GetInvoiceNum5000AndDate4_13_2018AndCost120()
+                //  SELECT * FROM Invoices WHERE InvoiceNum = 5000 AND InvoiceDate = #4/13/2018# AND TotalCost = 120
+                //Note: Query works but doesn't return anything due to no invoices existing with that information
+                string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = 5000 AND InvoiceDate = #4/13/2018# AND TotalCost = 120";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with: Cost 1200
+        /// </summary>
+        /// <returns></returns>
+        public string GetInvoiceCost1200()
+        {
+            try
+            {
+                //GetInvoiceCost1200()
+                //  SELECT * FROM Invoices WHERE TotalCost = 1200
+                //Note: Query works but doesn't return anything due to no invoices existing with that information
+                string sSQL = "SELECT * FROM Invoices WHERE TotalCost = 1200";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with: Cost 1300, and Date 4/13/2018
+        /// </summary>
+        /// <returns></returns>
+        public string GetInvoiceCost1300AndDate4_13_2018()
+        {
+            try
+            {
+                //GetInvoiceCost1300AndDate4_13_2018()
+                //  SELECT * FROM Invoices WHERE TotalCost = 1300 and InvoiceDate = #4/13/2018#
+                //Note: Query works but doesn't return anything due to no invoices existing with that information
+                string sSQL = "SELECT * FROM Invoices WHERE TotalCost = 1300 and InvoiceDate = #4/13/2018#";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with: Date 4/13/2018
+        /// </summary>
+        /// <returns></returns>
+        public string GetInvoiceDate4_13_2018()
+        {
+            try
+            {
+                //GetInvoiceDate4_13_2018()
+                //  SELECT * FROM Invoices WHERE InvoiceDate = #4/13/2018#
+                //Note: Query works but doesn't return anything due to no invoices existing with that information
+                string sSQL = "SELECT * FROM Invoices WHERE InvoiceDate = #4/13/2018#";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with: Distinct Num
+        /// </summary>
+        /// <returns></returns>
+        public string GetDistinctInvoiceNum()
+        {
+            try
+            {
+                //GetDistinctInvoiceNum()
+                //  SELECT DISTINCT(InvoiceNum) From Invoices order by InvoiceNum
+                string sSQL = "SELECT DISTINCT(InvoiceNum) From Invoices order by InvoiceNum";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with: Distinct Date
+        /// </summary>
+        /// <returns></returns>
+        public string GetDistinctInvoiceDate()
+        {
+            try
+            {
+                //GetDistinctInvoiceDate()
+                //  SELECT DISTINCT(InvoiceDate) From Invoices order by InvoiceDate
+                string sSQL = "SELECT DISTINCT(InvoiceDate) From Invoices order by InvoiceDate";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// This SQL returns all invoices with: Distinct Cost
+        /// </summary>
+        /// <returns></returns>
+        public string GetDistinctInvoiceCost()
+        {
+            try
+            {
+                //GetDistinctInvoiceCost()
+                //  SELECT DISTINCT(TotalCost) From Invoices order by TotalCost
+                string sSQL = "SELECT DISTINCT(TotalCost) From Invoices order by TotalCost";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
     }
 }
--- Search/wndSearch.xaml.cs
+++ Search/wndSearch.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GroupProject.Main;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,12 +21,56 @@ namespace GroupProject.Search
     /// </summary>
     public partial class wndSearch : Window
     {
+        //sSelectedInvoiceID - Holds the invoice ID if the user selected one, and zero if no invoice is selected
+        //Data will be passed around with the following property that the main window can access:
+            //SelectedInvoiceID - Property main window can access to get the selected invoice ID
+
+        #region Properties
+        /// <summary>
+        /// Public property for the sSelectedInvoiceID
+        /// </summary>
+        public string SelectedInvoiceID {  get; set; }
+        #endregion
+
         public wndSearch()
         {
             InitializeComponent();
         }
 
-        //sSelectedInvoiceID - Holds the invoice ID if the user selected one, and zero if no invoice is selected
-        //SelectedInvoiceID - Property main window can access to get the selected invoice ID
+        private void clearFilterBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Hide current opened window
+                this.Hide();
+                //Show the main window
+                wndMain mainWindow = new wndMain();
+                mainWindow.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+
+        }
+
+        private void selectInvoiceBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Hide current opened window
+                this.Hide();
+                //Show the main window
+                wndMain mainWindow = new wndMain();
+                mainWindow.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+
+            //When SelectInvoiceBTN is pressed the invoice number will be passed to main as SelectedInvoiceID (see comments at top)
+            //When SelectInvoiceBTN is pressed we will update/refresh the form main when the window is closed
+        }
     }
 }
