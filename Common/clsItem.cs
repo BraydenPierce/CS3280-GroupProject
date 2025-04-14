2d1cbaf28516a4356bf753b485b4a2c23b498cc2
BraydenPierce <braydenpierce@mail.weber.edu>
2025-04-14 10:29:30 -0600

    Wrote common file clsItem.cs

--- Common/clsItem.cs
+++ Common/clsItem.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +11,66 @@ namespace GroupProject.Common
     internal class clsItem
     {
         //All variables to be retrieved from database
-        //string ItemCode to store unique item ID
-        //string ItemDesc to store description of item
-        //double Cost to store cost of item
+        /// <summary>
+        /// string ItemCode to store unique item ID
+        /// </summary>
+        private string sItemCode;
+        /// <summary>
+        /// string ItemDesc to store description of item
+        /// </summary>
+        private string sItemDesc;
+        /// <summary>
+        /// double Cost to store cost of item
+        /// </summary>
+        private double dItemCost;
 
-        //public clsItem(ID, Desc, Cost) constructor builds new item based off information passed in
+        /// <summary>
+        /// get/set for string variable itemCode
+        /// </summary>
+        public string ItemCode
+        {
+            get { return sItemCode; }
+            set { sItemCode = value; }
+        }
+        
+        /// <summary>
+        /// get/set for string variable itemDesc
+        /// </summary>
+        public string ItemDesc
+        {
+            get { return sItemDesc; }
+            set { sItemDesc = value; }
+        }
+
+        /// <summary>
+        /// get/set for double variable cost
+        /// </summary>
+        public double Cost
+        {
+            get { return dItemCost; }
+            set { dItemCost = value; }
+        }
+
+        /// <summary>
+        /// public clsItem(ID, Desc, Cost) constructor builds new item based off information passed in
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="Desc"></param>
+        /// <param name="Cost"></param>
+        public clsItem(string sID, string sDesc, double dCost)
+        {
+
+            try
+            {
+                sItemCode = sID;
+                sItemDesc = sDesc;
+                dItemCost = dCost;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
     }
 }
