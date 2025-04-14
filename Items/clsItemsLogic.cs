1334b37d641af532a826d26d7ebb3ea5d41d20ba
BraydenPierce <braydenpierce@mail.weber.edu>
2025-04-14 10:43:29 -0600

    Added the clsItemsLogic.cs class and linked it to Invoice.mdb

--- Items/clsItemsLogic.cs
+++ Items/clsItemsLogic.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GroupProject.Common;
 
 namespace GroupProject.Items
 {
     class clsItemsLogic
     {
         //public List<clsItem.cs> GetItems()
+        public List<clsItem> GetItems()
     }
 }
